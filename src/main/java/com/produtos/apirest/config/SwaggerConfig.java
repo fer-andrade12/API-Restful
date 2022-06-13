097c6d9643487768b22ba1436364a2493588dbe4
fer-andrade12 <fer.developer@outlook.com>
2022-06-13 15:24:03 -0300

    faltava arrumar o logs do swagger

--- src/main/java/com/produtos/apirest/config/SwaggerConfig.java
+++ src/main/java/com/produtos/apirest/config/SwaggerConfig.java
@@ -28,7 +28,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
   private ApiInfo metaData() {
     return new ApiInfoBuilder()
         .title("Spring Boot REST API")
-        .description("\"Spring Boot API REST PRODUTOS \"")
+        .description("Spring Boot API REST PRODUTOS")
         .version("1.0.0")
         .license("Apache License Version 2.0")
         .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
